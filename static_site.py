280137e0808f5bd1aa23b42318a8248830de7cb9
Jacques Frechet <zaphod-git@toastball.net>
2023-04-07 22:16:33 -0700

    wip

--- static_site.py
+++ static_site.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+import functools;
+import mimetypes;
 import os;
+import re;
 import shlex;
 import shutil;
 import subprocess;
@@ -9,7 +12,10 @@ import sys;
 
 RCS_URL = f'https://ftp.gnu.org/gnu/rcs/rcs-5.10.1.tar.lz'
 
+URLS = ['/toast/']
+
 mydir = os.path.dirname(__file__)
+outdir = os.path.join(mydir, 'site_root')
 
 
 def prep_rcs():
@@ -41,8 +47,95 @@ def prep_rcs():
     assert shutil.which('co')
 
 
+def get_script():
+    proc = subprocess.run(
+        ['co', '-q', '-p', os.path.join(mydir, 'www')],
+        check=True,
+        stdout=subprocess.PIPE,
+    )
+    return proc.stdout.replace(b'/home/zaphod/toast/', f'{os.path.abspath(mydir)}/'.encode('utf8'))
+
+
+class Page:
+    def __init__(self, url, response):
+        self.url = url
+        self.response = response
+
+    @functools.cached_property
+    def header_len(self):
+        result = self.response.find(b'\n\n')
+        assert result >= 0, self.response
+        return result
+
+    @property
+    def body(self):
+        return self.response[self.header_len+2:]
+
+    @functools.cached_property
+    def header_dict(self):
+        result = {}
+        for line in header.decode('utf8').splitlines():
+            k, v = line.split(':', 1)
+            result[k.strip().lower()] = v.strip()
+        return result
+
+    @property
+    def out_path(self):
+        mime_type = self.header_dict.get('content-type')
+        suffix = mimetypes.guess_extension(mime_type) or '' if mime_type else ''
+        if self.url.endswith(suffix):
+            suffix = ''
+        assert self.url.startswith('/'), url
+        index = 'index' if self.url.endswith('/') else ''
+        return f'{outdir}{self.url}{index}{suffix}'
+
+    @property
+    def links(self):
+        return ()  # XXX TODO implement
+
+
+class Fetcher:
+    def __init__(self, script=None):
+        self.script = get_script() if script is None else script
+
+    def fetch_page(self, url):
+        m = re.fullmatch(r'(/toast)(/.*)', url)
+        assert m, url
+        env = {
+            'SCRIPT_NAME': m.group(1),
+            'PATH_INFO': m.group(2),
+            'SERVER_NAME': 'XXX',
+            **os.environ,
+        }
+        proc = subprocess.run(
+            ['perl'],
+            check=True,
+            input=self.script,
+            stdout=subprocess.PIPE,
+            cwd=mydir,
+            env=env,
+        )
+        return Page(url=url, response=proc.stdout)
+
+    def emit_html(self, urls=URLS):
+        seen = set(urls)
+        pending = list(reversed(urls))
+        while pending:
+            url = pending.pop()
+            page = self.fetch_page(url)
+            os.makedirs(os.path.dirname(page.out_path), exist_ok=True)
+            with open(page.out_path, 'wb') as f:
+                f.write(page.body)
+            for u in page.links:
+                if u in seen:
+                    continue
+                seen.add(u)
+                pending.append(u)
+
+
 def main():
     prep_rcs()
+    Fetcher().emit_html()
 
 
 if __name__ == '__main__':
